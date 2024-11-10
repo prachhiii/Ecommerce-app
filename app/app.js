a45414f0bfd24c55ae875bdfa738d00928a513f5
prachhiii <86996621+prachhiii@users.noreply.github.com>
2024-11-10 20:02:15 +0545

    Add files via upload

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View} from "react-native";
+import { useDeviceOrientation, useImageDimensions} from "@react-native-community/hooks";
+
+export default function app () {
+    return (
+        <View style={{backgroundColor:"red"}} >
+            <button title="hiree meeee"/>
+        <View style={{backgroundColor:"pink"}}/>
+    
+        </View>
+    )
+}
